f724355f81e6c22c29772ce118886f30e0269fe8
Sarthak Gupta <sarthakuniverse007@gmail.com>
2026-09-03 13:38:08 +0530

    Time: 1 ms (6.46%), Space: 8.6 MB (39.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,14 +5,28 @@ public:
     //     return climbStairs(n-1)+climbStairs(n-2);        
     // }
 
-    int helper(int n,vector<int> &dp){
-        if(n<=2) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
-    }
+    //MEMOIZATION
+
+    // int helper(int n,vector<int> &dp){
+    //     if(n<=2) return n;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    // }
+
+    // int climbStairs(int n){
+    //     vector<int>dp(n+1,-1);
+    //     return helper(n,dp);
+    // }
 
     int climbStairs(int n){
-        vector<int>dp(n+1,-1);
-        return helper(n,dp);
+        if(n<=2) return n;
+        vector<int> dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+         
+        }
+        return dp[n];
     }
 };
