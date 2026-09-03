741ec2a2a96b1ac84ea999c8956236be4ae8c15c
Sarthak Gupta <sarthakuniverse007@gmail.com>
2026-09-03 14:40:36 +0530

    Time: 0 ms (100%), Space: 10.2 MB (72.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,19 +12,47 @@ public:
     //     return max(sum1,sum2);
     // }
 
-    int robmaxmoney(int n,vector<int> &nums, vector<int> &dp){
-        if(n<0) return 0;
-        if(n==0) return dp[0]=nums[0];
-        if(dp[n]!=-1) return dp[n];
-        int robcurrentindex=nums[n]+robmaxmoney(n-2,nums,dp);
-        int skipcurrentindex=robmaxmoney(n-1,nums,dp);
-        return dp[n]=max(robcurrentindex,skipcurrentindex);
-    }
+    // int robmaxmoney(int n,vector<int> &nums, vector<int> &dp){
+    //     if(n<0) return 0;
+    //     if(n==0) return dp[0]=nums[0];
+    //     if(dp[n]!=-1) return dp[n];
+    //     int robcurrentindex=nums[n]+robmaxmoney(n-2,nums,dp);
+    //     int skipcurrentindex=robmaxmoney(n-1,nums,dp);
+    //     return dp[n]=max(robcurrentindex,skipcurrentindex);
+    // }
    
-    int rob(vector<int>& nums) {
+    // int rob(vector<int>& nums) {
+    //     int n=nums.size();
+    //     vector<int>dp(n,-1);
+    //     return robmaxmoney(n-1,nums,dp);
+    // }
+
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1) return nums[0];
+//         vector<int> dp(n);
+//         dp[0]=nums[0];
+//         dp[1]=max(nums[1],nums[0]);
+//         for(int i=2;i<n-1;i++){
+//             int robcurrentindex=nums[i]+dp[i-2];
+//             int skipcurrentindex=dp[i-1];
+//             dp[i]=max(robcurrentindex,skipcurrentindex);
+//         }
+//         return dp[n-1];
+//     }
+    
+    int rob(vector<int> &nums){
+
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return robmaxmoney(n-1,nums,dp);
+        if(n==1)return nums[0];
+        int a=nums[0];
+        int b=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            int c=max(nums[i]+a,b);
+            a=b;
+            b=c;
+        }
+        return b;
     }
 
 
